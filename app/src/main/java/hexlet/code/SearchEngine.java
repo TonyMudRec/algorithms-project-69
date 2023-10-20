a1b286015cc75ef1f3f777250e6d65b40c14af81
mudrec <sm1le88man@gmail.com>
2023-10-20 10:17:23 +0300

    add "not clear" search

--- app/src/main/java/hexlet/code/SearchEngine.java
+++ app/src/main/java/hexlet/code/SearchEngine.java
@@ -1,31 +1,69 @@
 package hexlet.code;
 
 import java.util.ArrayList;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
-import org.apache.commons.lang3.StringUtils;
+import java.util.LinkedList;
+import java.util.regex.MatchResult;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 public class SearchEngine {
+
+    private static final int BASIC_TEXT_CAPACITY = 20;
 
     public static List<String> search(List<Map<String, String>> docs, String pattern) {
         if (docs.isEmpty()) {
             return new ArrayList<>();
         }
-        List<String> result = new LinkedList<>();
-        String[] words;
+        var relevanceList = getRelevanceList(BASIC_TEXT_CAPACITY);
+
+        var relevanceCounter = 0;
+
         for (Map<String, String> doc : docs) {
             if (doc.isEmpty()) {
                 continue;
             }
-            words = doc.get("text").split(" ");
-            for (String word : words) {
-                if (word.equals(pattern)) {
-                    result.add(doc.get("id"));
-                    break;
+            var words = doc.get("text").split(" ");
+            var partsOfPattern = pattern.split(" ");
+            for (var word : words) {
+                for (var partOfPattern : partsOfPattern) {
+                    if (wordConvert(word).equals(partOfPattern)) {
+                        relevanceCounter++;
+                    }
                 }
+            }
+            if (relevanceCounter > 0) {
+                relevanceList.get(relevanceCounter).add(doc.get("id"));
+                relevanceCounter = 0;
+            }
+        }
+        return buildResult(relevanceList);
+    }
+
+    public static List<String> buildResult(List<LinkedList<String>> list) {
+        LinkedList<String> result = new LinkedList<>();
+        for (var docNames : list) {
+            for (var doc : docNames) {
+                result.addFirst(doc);
             }
         }
         return result;
+    }
+
+    public static List<LinkedList<String>> getRelevanceList(int size) {
+        List<LinkedList<String>> list = new ArrayList<>();
+        for (int i = 0; i < size; i++) {
+            list.add(new LinkedList<>());
+        }
+        return list;
+    }
+
+    public static String wordConvert(String word) {
+        return Pattern.compile("\\w+")
+                .matcher(word)
+                .results()
+                .map(MatchResult::group)
+                .collect(Collectors.joining());
     }
 }
--- app/src/test/java/hexlet/code/SearchEngineTest.java
+++ app/src/test/java/hexlet/code/SearchEngineTest.java
@@ -6,31 +6,49 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
-import static org.junit.jupiter.api.Assertions.*;
 import static org.assertj.core.api.Assertions.*;
 
 class SearchEngineTest {
-    @Test
-    void searchTest() {
         // Текст документов
-        var doc1 = "I can't shoot straight unless I've had a pint!";
-        var doc2 = "Don't shoot shoot shoot that thing at me.";
-        var doc3 = "I'm your shooter.";
+        String doc1 = "I can't shoot straight unless I've had a pint!";
+        String doc2 = "Don't shoot shoot shoot that thing at me.";
+        String doc3 = "I'm your shooter. It's me.";
+        String doc4 = "shoot me, shoot me, shoot me!";
 
         // создание документа
         // документ имеет два атрибута "id" и "text"
         List<Map<String, String>> docs = List.of(
                 Map.of("id", "doc1", "text", doc1),
                 Map.of("id", "doc2", "text", doc2),
-                Map.of("id", "doc3", "text", doc3)
+                Map.of("id", "doc3", "text", doc3),
+                Map.of("id", "doc4", "text", doc4)
         );
 
-        // поисковый движок проводит поиск
-        List<String> result = SearchEngine.search(docs, "shoot");
-        List<String> correct = List.of("doc1", "doc2");
-        assertThat(result).isEqualTo(correct);
 
-        List<String> result1 = SearchEngine.search(new ArrayList<Map<String, String>>(), "shoot");
-        assertThat(result1).isNullOrEmpty();
+    @Test
+    void searchOneWordTest() {
+        List<String> result = SearchEngine.search(docs, "pint");
+        List<String> correct = List.of("doc1");
+        assertThat(result).isEqualTo(correct);
+    }
+
+    @Test
+    void searchMultipleWordsTest() {
+        List<String> result = SearchEngine.search(docs, "shoot at me");
+        List<String> correct = List.of("doc4", "doc2", "doc3", "doc1");
+        assertThat(result).isEqualTo(correct);
+    }
+
+    @Test
+    void searchWithEmptyDocTest() {
+        List<String> result = SearchEngine.search(new ArrayList<Map<String, String>>(), "shoot");
+        assertThat(result).isNullOrEmpty();
+    }
+
+    @Test
+    void searchWithRelevanceOrder() {
+        List<String> result = SearchEngine.search(docs, "shoot");
+        List<String> correct = List.of("doc4", "doc2", "doc1");
+        assertThat(result).isEqualTo(correct);
     }
 }
