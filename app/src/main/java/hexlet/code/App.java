6adfe58fd89273c1796e5ca338056010d5cc0618
mudrec <sm1le88man@gmail.com>
2023-11-16 17:54:08 +0300

    added description to README

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,22 @@
 package hexlet.code;
 
+import java.util.List;
+import java.util.Map;
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("It is Search Engine main method!");
+        String doc1 = "I can't shoot straight unless I've had a pint! Pour me!";
+        String doc2 = "Don't shoot shoot shoot that thing at me.";
+        String doc3 = "I'm your shooter. It's me.";
+        String doc4 = "shoot me, shoot me!";
+
+        List<Map<String, String>> docs = List.of(
+                Map.of("id", "doc1", "text", doc1),
+                Map.of("id", "doc2", "text", doc2),
+                Map.of("id", "doc3", "text", doc3),
+                Map.of("id", "doc4", "text", doc4)
+        );
+
+        System.out.println(SearchEngine.search(docs, "shoot me")); // [doc4, doc2, doc1, doc3]
     }
 }
